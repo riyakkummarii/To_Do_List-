7b0091cc140500b51aeeca8f14acb53863ca2303
Riya Kumari <riyakkummarii@gmail.com>
2020-09-16 16:07:59 +0530

    Version 2- When clicking the checkbox item is deleted.Items are stored in local MongoDB database.

--- app.js
+++ app.js
@@ -1,56 +1,163 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date=require(__dirname+"/date.js");
+const mongoose = require("mongoose");
+const _= require("lodash");
 
-//console.log(date);
-//console.log(date());
-
-const items=[];
-const workItems=[];
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs'); //This assumes a views directory containing an index.ejs page.Since we are making a to do list we name it list.ejs
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("item", itemsSchema);
+
+
+const item1 = new Item({
+  name: "Welcome to your ToDOList!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this checkbox to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", function(req, res) {
-
-const day =date.getDate();
-
-//renders to list.ejs page
-  res.render("list", {
-    listTitle: day,
-    newListItems : items
+  Item.find({}, function(err, foundItems) {
+    if (err) {
+      console.log(err);
+    } else if (foundItems.length === 0) {
+      
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Sucessfully saved default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      }); 
+    }
   });
+
 });
+app.post("/", function(req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.listButton;
 
+  const item = new Item({
+    name: itemName
+  });
 
-
-app.post("/",function(req, res)
-{
-   const item= req.body.newItem;
-   //console.log(req.body.listButton);
-  if(req.body.listButton === "Work")// after white space it ignores value so work is given instead of worklist
-  {
-    workItems.push(item);
-
-    res.redirect("/work");
-  }
-  else {
-    items.push(item);
-
+  if (listName === "Today") {
+    item.save();
     res.redirect("/");
+  } else {
+
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      if (!err) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      }
+
+    });
+  }
+});
+
+app.post("/delete", function(req, res) {
+ 
+  const checkedItemID = req.body.checked;
+
+  const listName_ = req.body.listName;
+
+  if (listName_ === "Today") {
+    Item.findByIdAndRemove(checkedItemID, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Sucessfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  }
+//To delete items of Custom List
+  else {
+    List.findOneAndUpdate({
+      name: listName_
+    }, {
+      $pull: {
+      items:{  _id: checkedItemID}
+      }
+    }, function(err, foundList) {
+      if (!err) {
+
+        res.redirect("/"+ listName_);
+      }
+      else {console.log(err);}
+    });
   }
 
+
+
 });
 
-app.get("/work",function (req, res){
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems : workItems
+
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    }
   });
 });
-app.get("/about",function (req, res){
+
+
+
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
